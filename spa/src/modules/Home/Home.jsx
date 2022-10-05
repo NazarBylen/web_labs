b03c413fea861702c1b16f5525995273e5153cdf
unknown <nazar.bylen.ir.2021@lpnu.ua>
2022-10-05 21:09:38 +0300

    home page:view more button functionality

--- spa/src/modules/Home/Home.jsx
+++ spa/src/modules/Home/Home.jsx
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+
 import './styles.scss'
 import {Card} from '../../components'
 import { Button } from '../../components'
@@ -7,7 +9,7 @@ import threePlusOne from '../../assets/img/threePlusOne.png'
 import twentyPercents from '../../assets/img/twentypercents.png'
 import twoPlusOne from '../../assets/img/twoPlusOne.png'
 
-const cards = [
+const cardsData = [
     {
         id: 1,
         image: threePlusOne,
@@ -26,11 +28,51 @@ const cards = [
         title: 'Акція 2+1!',
         desc: 'Замовляй 3 піци та отримуй одну з них безкоштовно!',
     },
+    {
+        id: 4,
+        image: threePlusOne,
+        title: '11111Акція 3+1!',
+        desc: 'Замовляй 3 піци та отримай 4-ту в подарунок!',
+    },
+    {
+        id: 5,
+        image: twentyPercents,
+        title: '11111-20% за піцу',
+        desc: 'Забирай замовлення та отримуй знижку 20%!',
+    },
+    {
+        id: 6,
+        image: twoPlusOne,
+        title: '11111Акція 2+1!',
+        desc: 'Замовляй 3 піци та отримуй одну з них безкоштовно!',
+    },
 ];
 
 const Home = () => {
+    const [from, setFrom] = useState(0);
+    const [cards, setCards] = useState([])
+    const perPage = 3;
+    const lastPage = 6;
+
+    const getCards = () => {
+        const to = from === 0 ? perPage : perPage+from
+        const fetchedData = cardsData.slice(from, to);
+        setCards([...cards, ...fetchedData]);
+        setFrom(perPage+from);
+    }
+
+    const loadMore = () => {
+        console.log('load - more');
+        getCards();
+    }
+
+    useEffect(() => {
+        getCards();
+    }, [])
+
     return (
         <div>
+            <div>{from}</div>
            <div className="row">
                <div className="col">
                    <PromoCard
@@ -41,7 +83,7 @@ const Home = () => {
            <div className="row justify-content-between">
                {cards.map(item => {
                    return (
-                       <div key={item.id} className="col">
+                       <div key={item.id} className="col-4">
                            <Card
                                image={item.image}
                                title={item.title}
@@ -51,9 +93,13 @@ const Home = () => {
                    )
                })}
            </div>
-            <div className="button-wrapper">
-                <Button text="View more" />
-            </div>
+            {
+                from < lastPage ? (
+                    <div className="button-wrapper">
+                        <Button text="View more" handleClick={loadMore}/>
+                    </div>
+                ) : null
+            }
         </div>
     );
 };
