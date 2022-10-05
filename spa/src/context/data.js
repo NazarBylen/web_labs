f413a77c5190d806b8673d9cfced7d6c2ea7c581
unknown <nazar.bylen.ir.2021@lpnu.ua>
2022-10-05 20:14:51 +0300

    context created

--- spa/src/context/data.js
+++ spa/src/context/data.js
@@ -1,8 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export const data = [
     {
-        id: uuidv4(),
+        id: "1",
         name: 'Карне',
         desc: 'Шинка, салямі мілано, куряче філе, бекон, сир моцарела, неаполітанський соус, орегано',
         price: 230,
@@ -10,7 +8,7 @@ export const data = [
         img: 'https://mapizza.com.ua/wp-content/uploads/2021/11/1-4-350x233.png'
     },
     {
-        id: uuidv4(),
+        id: "2",
         name: 'Кватро Формаджі',
         desc: 'Сир Моцарелла, сир Рікота, сир  Пармезан, сир Дор Блю, вершковий соус.',
         price: 229,
@@ -18,7 +16,7 @@ export const data = [
         img: 'https://mapizza.com.ua/wp-content/uploads/2021/11/formaji-350x233.jpg'
     },
     {
-        id: uuidv4(),
+        id: "3",
         name: 'Верона',
         desc: 'Шинка, гриби печериці, сир пармезан, трюфельна олія, базилік, сир моцарела, вершковий соус.',
         price: 223,
@@ -26,7 +24,7 @@ export const data = [
         img: 'https://mapizza.com.ua/wp-content/uploads/2021/11/verona-1.jpg'
     },
     {
-        id: uuidv4(),
+        id: "4",
         name: 'Капрічоза',
         desc: 'Прошуто крудо, шинка, артишоки, сир моцарела, гриби печериці, свіжий базилік, неаполітанський соус',
         price: 223,
@@ -34,7 +32,7 @@ export const data = [
         img: 'https://mapizza.com.ua/wp-content/uploads/2021/09/2.png'
     },
     {
-        id: uuidv4(),
+        id: "5",
         name: 'Іспана',
         desc: 'Салямі чорізо, бекон, ковбаски, кукурудза, сир моцарела, неаполітанський соус.',
         price: 216,
@@ -42,7 +40,7 @@ export const data = [
         img: 'https://mapizza.com.ua/wp-content/uploads/2021/09/ispana-1.jpg'
     },
     {
-        id: uuidv4(),
+        id: "6",
         name: '3 сири з грушкою',
         desc: 'Сир моцарелла двох видів, сир Дор Блю, груша, мед, грецький горіх, вершковий соус.',
         price: 214,
